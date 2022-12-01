52cb6d203ab2209d9629f19324e9356e77364706
Chris Porter <chris.porter@uplight.com>
2022-11-30 21:16:13 -0800

    Cleanup solution for reuse in future puzzles

--- day1/part1.py
+++ day1/part1.py
@@ -1,14 +1,20 @@
 
-with open('day1/input.txt') as f:
-    elf_total = 0
-    max_total = 0
-    
-    for line in f.readlines():
-        if line.strip() == '':
-            elf_total = 0
-        else:
-            val = int(line)
-            elf_total += val
-            max_total = max(max_total, elf_total)
+def run(filename):
+    with open(filename) as f:
+        elf_total = 0
+        max_total = 0
+        
+        for line in f.readlines():
+            if line.strip() == '':
+                elf_total = 0
+            else:
+                val = int(line)
+                elf_total += val
+                max_total = max(max_total, elf_total)
 
-    print(max_total)
+        return max_total
+
+
+assert run('day1/example.txt') == 24000
+
+print(run('day1/input.txt'))
--- day1/part2.py
+++ day1/part2.py
@@ -1,17 +1,20 @@
 
+def run(filename):
+    with open(filename) as f:
+        elf_total = 0
+        max_total = 0
+        top3 = []
+        for line in f.readlines():
+            if line.strip() == '':
+                elf_total = 0
+            else:
+                val = int(line)
+                elf_total += val
+                top3.append(elf_total)
+                top3.sort()
+                top3 = top3[-3:]
 
-with open('day1/input.txt') as f:
-    elf_total = 0
-    max_total = 0
-    top3 = []
-    for line in f.readlines():
-        if line.strip() == '':
-            elf_total = 0
-        else:
-            val = int(line)
-            elf_total += val
-            top3.append(elf_total)
-            top3.sort()
-            top3 = top3[-3:]
 
-    print(sum(top3))
+assert run('day1/example.txt') == 45000
+
+print(run('day1/input.txt'))

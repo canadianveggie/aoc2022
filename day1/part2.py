d340109323f4e131306ec29df10c0f853348afe8
Chris Porter <chris.porter@uplight.com>
2022-11-30 21:35:34 -0800

    More cleanup

--- day1/part2.py
+++ day1/part2.py
@@ -2,17 +2,22 @@
 def run(filename):
     with open(filename) as f:
         elf_total = 0
-        max_total = 0
         top3 = []
         for line in f.readlines():
             if line.strip() == '':
+                top3 = adjust_top3(top3, elf_total)
                 elf_total = 0
             else:
                 val = int(line)
                 elf_total += val
-                top3.append(elf_total)
-                top3.sort()
-                top3 = top3[-3:]
+        top3 = adjust_top3(top3, elf_total)
+        return sum(top3)
+
+
+def adjust_top3(top3, elf_total):
+    top3.append(elf_total)
+    top3.sort()
+    return top3[-3:]
 
 
 assert run('day1/example.txt') == 45000
